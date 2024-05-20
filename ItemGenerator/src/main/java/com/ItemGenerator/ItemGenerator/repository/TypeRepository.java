df81a6911d1aa0801efcaefb77ccd039e996085b
Vortex <129182012+goldslu1@users.noreply.github.com>
2024-05-20 23:43:27 +0200

    Delete ItemGenerator directory

--- ItemGenerator/src/main/java/com/ItemGenerator/ItemGenerator/repository/TypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ItemGenerator.ItemGenerator.repository;
-
-import com.ItemGenerator.ItemGenerator.model.Type;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRepository extends JpaRepository<Type, Long> {
-
-}
